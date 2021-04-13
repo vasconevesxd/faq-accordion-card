6e7a8eb27084a0e44d31cd1d546a5f48078776c3
Vasco Neves <57102041+vasconevesxd@users.noreply.github.com>
2021-04-13 23:38:31 +0100

    Height card, js text and image and box image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var buttonAccordion = document.getElementsByClassName("button");
 var bodyContent = document.getElementsByClassName("card__body");
+var titles = document.getElementsByTagName("h3");
+var images = document.getElementsByClassName("icon-arrow");
 
 for (i = 0; i < buttonAccordion.length; i++) {
     buttonAccordion[i].addEventListener('click', toggleItem, false);
@@ -7,8 +9,18 @@ for (i = 0; i < buttonAccordion.length; i++) {
 
 function toggleItem() {
 
+    for (let index = 0; index < bodyContent.length; index++) {
+        bodyContent[index].classList.remove('open');
+        images[index].style.transform = "rotate(0deg)";
+        titles[index].style.fontWeight = "400"; 
+    }
+    var title = this.getElementsByTagName("h3");
+    var image = this.getElementsByClassName("icon-arrow");
     var panel = this.nextElementSibling;
-    console.log(panel);
+
+    title[0].style.fontWeight = "900"; 
+    image[0].style.transform = "rotate(180deg)";
     panel.classList.toggle('open');
- 
+
+
 }
